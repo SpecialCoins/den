c013f9e75cf51dc701869c2d19701340af3299bc
SpecialCoins <>
2020-04-22 03:40:31 +0300

    Update blockchain.cpp

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -396,7 +396,7 @@ UniValue mempoolToJSON(bool fVerbose = false)
     }
 }
 
-static UniValue clearmempool(const UniValue& params, bool fHelp)
+UniValue clearmempool(const UniValue& params, bool fHelp)
 {
     if (fHelp || params.size() > 1)
         throw std::runtime_error(
