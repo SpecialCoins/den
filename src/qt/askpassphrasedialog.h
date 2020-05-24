7059d265e421d6d78998c657c88b5a5d3235c6bd
SpecialCoins <>
2020-05-24 20:45:35 +0300

    Update askpassphrasedialog.h

--- src/qt/askpassphrasedialog.h
+++ src/qt/askpassphrasedialog.h
@@ -63,6 +63,7 @@ private:
     bool fCapsLock;
     SecureString newpassCache = "";
 
+    void updateWarningsLabel();
     void run(int type) override;
     void onError(QString error, int type) override;
     QCheckBox *btnWatch;
