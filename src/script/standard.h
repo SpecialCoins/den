279cbbc2680d9cbc426497fa93b2b1da8e524f00
SpecialCoins <>
2020-05-24 17:01:34 +0300

    Update standard.h

--- src/script/standard.h
+++ src/script/standard.h
@@ -78,7 +78,7 @@ public:
  *  * CNoDestination: no destination set
  *  * CKeyID: TX_PUBKEYHASH destination
  *  * CScriptID: TX_SCRIPTHASH destination
- *  A CTxDestination is the internal data type encoded in a PIVX address
+ *  A CTxDestination is the internal data type encoded in a BCZ address
  */
 typedef boost::variant<CNoDestination, CKeyID, CScriptID> CTxDestination;
 
