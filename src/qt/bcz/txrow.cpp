b118e4b0079351ba8a4f2c4c63e46a89f26483dc
SpecialCoins <>
2020-05-28 21:22:31 +0300

    Update txrow.cpp

--- src/qt/bcz/txrow.cpp
+++ src/qt/bcz/txrow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019-2020 The BCZ developers
+// Copyright (c) 2020 The BCZ developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -15,41 +15,48 @@ TxRow::TxRow(QWidget *parent) :
     ui->setupUi(this);
 }
 
-void TxRow::init(bool isLightTheme) {
+void TxRow::init(bool isLightTheme)
+{
     setConfirmStatus(true);
     updateStatus(isLightTheme, false, false);
 }
 
-void TxRow::setConfirmStatus(bool isConfirm){
-    if(isConfirm){
+void TxRow::setConfirmStatus(bool isConfirm)
+{
+    if (isConfirm) {
         setCssProperty(ui->lblAddress, "text-list-body1");
         setCssProperty(ui->lblDate, "text-list-caption");
-    }else{
+    } else {
         setCssProperty(ui->lblAddress, "text-list-body-unconfirmed");
         setCssProperty(ui->lblDate,"text-list-caption-unconfirmed");
     }
 }
 
-void TxRow::updateStatus(bool isLightTheme, bool isHover, bool isSelected){
-    if(isLightTheme)
+void TxRow::updateStatus(bool isLightTheme, bool isHover, bool isSelected)
+{
+    if (isLightTheme)
         ui->lblDivisory->setStyleSheet("background-color:#908c96");
     else
         ui->lblDivisory->setStyleSheet("background-color:#40ffffff");
 }
 
-void TxRow::setDate(QDateTime date){
+void TxRow::setDate(QDateTime date)
+{
     ui->lblDate->setText(GUIUtil::dateTimeStr(date));
 }
 
-void TxRow::setLabel(QString str){
+void TxRow::setLabel(QString str)
+{
     ui->lblAddress->setText(str);
 }
 
-void TxRow::setAmount(QString str){
+void TxRow::setAmount(QString str)
+{
     ui->lblAmount->setText(str);
 }
 
-void TxRow::setType(bool isLightTheme, int type, bool isConfirmed){
+void TxRow::setType(bool isLightTheme, int type, bool isConfirmed)
+{
     QString path;
     QString css;
     bool sameIcon = false;
@@ -64,6 +71,9 @@ void TxRow::setType(bool isLightTheme, int type, bool isConfirmed){
         case TransactionRecord::RecvFromOther:
         case TransactionRecord::SendToAddress:
         case TransactionRecord::SendToOther:
+            path = "://ic-transaction-sent";
+            css = "text-list-amount-send";
+            break;
         case TransactionRecord::SendToSelf:
             path = "://ic-transaction-mint";
             css = "text-list-amount-send";
@@ -105,13 +115,14 @@ void TxRow::setType(bool isLightTheme, int type, bool isConfirmed){
         css = "text-list-amount-unconfirmed";
         path += "-inactive";
         setConfirmStatus(false);
-    }else{
+    } else {
         setConfirmStatus(true);
     }
     setCssProperty(ui->lblAmount, css, true);
     ui->icon->setIcon(QIcon(path));
 }
 
-TxRow::~TxRow(){
+TxRow::~TxRow()
+{
     delete ui;
 }
