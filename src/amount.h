1203251f7afdf9906286dcd809cd0baaec2185fb
SpecialCoins <>
2020-06-16 23:58:20 +0300

    Update amount.h

--- src/amount.h
+++ src/amount.h
@@ -20,7 +20,7 @@ typedef int64_t CAmount;
 static const CAmount COIN = 100000000;
 static const CAmount CENT = 1000000;
 
-static const CAmount MAX_MONEY_OUT = 3999999 * COIN;
+static const CAmount MAX_MONEY_OUT = 21000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY_OUT); }
 
 /**
