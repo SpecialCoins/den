2331ec047850b31ed47fec6494cd37521e709bf7
SpecialCoins <>
2020-06-17 07:40:26 +0300

    Update walletmodel.h

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -75,7 +75,7 @@ public:
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
+    inline void SerializationOp(Stream& s, Operation ser_action)
     {
         std::string sAddress = address.toStdString();
         std::string sLabel = label.toStdString();
@@ -85,8 +85,7 @@ public:
             paymentRequest.SerializeToString(&sPaymentRequest);
         std::string sAuthenticatedMerchant = authenticatedMerchant.toStdString();
 
-        READWRITE(this->nVersion);
-        nVersion = this->nVersion;
+        READWRITE(nVersion);
         READWRITE(sAddress);
         READWRITE(sLabel);
         READWRITE(amount);
