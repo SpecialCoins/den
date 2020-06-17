02041499ec5ea413eee93b260b21ec38f9833b2f
SpecialCoins <>
2020-06-17 19:46:07 +0300

    Update bcz_fr_FR.ts

--- src/qt/locale/bcz_fr_FR.ts
+++ src/qt/locale/bcz_fr_FR.ts
@@ -1453,7 +1453,7 @@ Il commencera automatiquement dès que le portefeuille aura assez de confirmatio
     </message>
     <message>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;A transaction of 5,000 BCZ will be made&lt;/p&gt;&lt;p&gt;to a new empty address in your wallet.&lt;/p&gt;&lt;p&gt;The Address is labeled under the master node's name.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Une transaction de 10 000 BCZ sera effectuée &lt;/p&gt;&lt;p&gt; vers une nouvelle adresse de votre partefeuille.&lt;/p&gt;&lt;p&gt; L'adresse est étiquettée avec le nom de masternode. &lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Une transaction de 5 000 BCZ sera effectuée &lt;/p&gt;&lt;p&gt; vers une nouvelle adresse de votre partefeuille.&lt;/p&gt;&lt;p&gt; L'adresse est étiquettée avec le nom de masternode. &lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <source>Set Master Node’s IP and port</source>
@@ -1683,7 +1683,7 @@ Confirmez-vous?</translation>
     </message>
     <message>
         <source>Not enough balance to create a masternode, 5,000 BCZ required.</source>
-        <translation>Fonds insuffisant pour créer un masternode, 10 000 BCZ sont requis.</translation>
+        <translation>Fonds insuffisant pour créer un masternode, 5 000 BCZ sont requis.</translation>
     </message>
     <message>
         <source>Error creating masternode</source>
