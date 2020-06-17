5f28e0c2d99bada24eefe4a7b22f1b38265c8ba0
SpecialCoins <>
2020-06-18 01:08:47 +0300

    xxxxxxx

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -37,11 +37,6 @@ const std::string CLIENT_NAME("PIVX Core");
  * finally CLIENT_VERSION_SUFFIX is added
  */
 
-//! First, include build.h if requested
-#ifdef HAVE_BUILD_INFO
-#include "obj/build.h"
-#endif
-
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
