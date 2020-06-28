6861a74b4b29b9e841ac11594e176d0d1104d060
SpecialCoins <>
2020-06-28 11:08:04 +0300

    Update wallet.cpp

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -11,6 +11,7 @@
 #include "init.h"
 #include "guiinterfaceutil.h"
 #include "masternode-payments.h"
+#include "policy/policy.h"
 #include "script/sign.h"
 #include "spork.h"
 #include "swifttx.h"    // mapTxLockReq
