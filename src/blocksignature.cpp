bcf86e4cc144dd7076fbfeaaee7ffc7d832bcb3e
SpecialCoins <>
2020-06-29 16:45:37 +0300

    Update blocksignature.cpp

--- src/blocksignature.cpp
+++ src/blocksignature.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020 The BCZ developers
+// Copyright (c) 2020 The BCZ developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -46,16 +46,7 @@ bool CheckBlockSignature(const CBlock& block)
     if (block.vchBlockSig.empty())
         return error("%s: vchBlockSig is empty!", __func__);
 
-    /** Each block is signed by the private key of the input that is staked. This can be either zBCZ or normal UTXO
-     *  zBCZ: Each zBCZ has a keypair associated with it. The serial number is a hash of the public key.
-     *  UTXO: The public key that signs must match the public key associated with the first utxo of the coinstake tx.
-     */
     CPubKey pubkey;
-    bool fzBCZStake = block.vtx[1].vin[0].IsZerocoinSpend();
-    if (fzBCZStake) {
-        libzerocoin::CoinSpend spend = TxInToZerocoinSpend(block.vtx[1].vin[0]);
-        pubkey = spend.getPubKey();
-    } else {
         txnouttype whichType;
         std::vector<valtype> vSolutions;
         const CTxOut& txout = block.vtx[1].vout[1];
@@ -71,7 +62,6 @@ bool CheckBlockSignature(const CBlock& block)
             start += 1 + (int) *(txin.scriptSig.begin()+start); // skip flag
             pubkey = CPubKey(txin.scriptSig.begin()+start+1, txin.scriptSig.end());
         }
-    }
 
     if (!pubkey.IsValid())
         return error("%s: invalid pubkey %s", __func__, HexStr(pubkey));
