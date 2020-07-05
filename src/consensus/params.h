7d5f1a26149724ef2fb0608a4c46a87d3c15f89f
SpecialCoins <>
2020-07-05 15:38:37 +0300

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -104,7 +104,7 @@ struct Params {
     bool MoneyRange(const CAmount& nValue) const { return (nValue >= 0 && nValue <= nMaxMoneyOut); }
     bool IsTimeProtocolV2(const int nHeight) const { return NetworkUpgradeActive(nHeight, UPGRADE_V4_0); }
 
-    int FutureBlockTimeDrift() const
+    int64_t FutureBlockTimeDrift() const
     {
         return nFutureTimeDrift;
     }
