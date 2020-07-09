e63f0e7d7578b84cf4bc868845671b0053354ea4
SpecialCoins <>
2020-07-09 12:32:59 +0300

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -51,7 +51,7 @@ const std::string CLIENT_NAME("BCZ Core");
     "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-g" commit
 
 #define BUILD_DESC_FROM_UNKNOWN(maj, min, rev, build) \
-    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-unk"
+    "v" DO_STRINGIZE(maj) "." DO_STRINGIZE(min) "." DO_STRINGIZE(rev) "." DO_STRINGIZE(build) "-travis"
 
 #ifndef BUILD_DESC
 #ifdef BUILD_SUFFIX
